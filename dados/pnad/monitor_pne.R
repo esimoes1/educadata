feaa8b3f228931938a92c49e46e76c0fe33a234e
professorvirtual <georgegomesf@gmail.com>
2017-04-08 11:02:22 -0300

    Update monitor_pne.R

--- dados/pnad/monitor_pne.R
+++ dados/pnad/monitor_pne.R
@@ -1,11 +1,9 @@
 # Dados de Monitoramento do PNE 2014-2024
 
-load("pnads.RData")
+# Carrega dados originais da PNAD
+load("../../bases/pnad/pnads.RData")
 
-library(data.table)
-library(bit64)
-
-# Empilha os anos e filtra as variáveis usadas no monitoramento do PNE
+# Empilha os anos e seleciona apenas as variáveis usadas no monitoramento do PNE
 pnads <- rbind(
   pnad11[c("UF","V0101","V8005","V3033","V3032","V3031","V4729","V0602","V0606","V0611","V6007","V6003","V0601","V4803")],
   pnad12[c("UF","V0101","V8005","V3033","V3032","V3031","V4729","V0602","V0606","V0611","V6007","V6003","V0601","V4803")],
@@ -14,4 +12,15 @@ pnads <- rbind(
   pnad15[c("UF","V0101","V8005","V3033","V3032","V3031","V4729","V0602","V0606","V0611","V6007","V6003","V0601","V4803")]
 )
 
-#
+# Seleciona apenas os sujeitos que possuem data de nascimento e converte colunas em variáveis isoladas (anexa o dataframe)
+attach(subset(pnads, V3032 %in% c(1:12)))
+
+pnad$idade_cne <- calcIdade(paste(
+  V3033, sprintf("%02d",V3032), V3031, sep="-"),
+  paste(substr(V0101,1,4),"-03-31",sep=""))
+
+# Desfaz o isolamento
+detach()
+
+
+
