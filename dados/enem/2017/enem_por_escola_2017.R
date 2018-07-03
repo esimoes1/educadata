9c61c56daf80a7793c86e8bd83a4231c902b39dd
George Gomes <georgegomesf@gmail.com>
2018-07-03 16:17:32 -0300

    Update enem_por_escola_2017.R

--- dados/enem/2017/enem_por_escola_2017.R
+++ dados/enem/2017/enem_por_escola_2017.R
@@ -1,4 +1,4 @@
-## Enem_por_escola.R - Produz os resultados do ENEM por Escola 2017
+## Enem_por_escola.R - Produz os resultados do ENEM 2017
 ## Escrito por: George Gomes (Fortaleza - Ceará)
 ## E-mail: georgegomesf@gmail.com
 
@@ -9,59 +9,72 @@ library(plyr)
 # Carrega dados ####
 load("MICRODADOS_ENEM_2017_escola.RData")
 
-# Trata e alinha a base de escolas acrescentando os rótulos das redes e número de matrículas
-redes <- data.frame(TP_DEPENDENCIA=c(1,2,3,4),DS_DEPENDENCIA=c("Federal","Estadual","Municipal","Privada"))
-escolas2017 <- merge(escolas2017,redes,by="TP_DEPENDENCIA")
-escolas2017 <- setNames(
-		merge(escolas2017,dcast(turmas2017,CO_ENTIDADE~"NU_MATRICULAS",value.var="NU_MATRICULAS",sum)),
-		c("id_escola","cd_dependencia","nm_escola","id_municipio","ds_dependencia","nr_matriculas")
-	)
-
-# Trata e alinha a base do ENEM calculando o número de inscritos e de participantes
+# Sinaliza informações sobre a particpação dos alunos
+enem2017[is.na(enem2017$TP_PRESENCA_CH),]$TP_PRESENCA_CH <- 0
 enem2017[is.na(enem2017$TP_PRESENCA_CN),]$TP_PRESENCA_CN <- 0
-enem2017[is.na(enem2017$TP_PRESENCA_LC),]$TP_PRESENCA_LC <- 0
-enem2017$ds_participante <- ifelse(enem2017$TP_PRESENCA_CN==1 & enem2017$TP_PRESENCA_CH==1 & enem2017$TP_PRESENCA_LC==1 & enem2017$TP_PRESENCA_MT==1,"participante","nao_participante")
-enem_part <- setNames(dcast(enem2017,CO_ESCOLA~ds_participante),c("id_escola","nr_nao_participantes","nr_participantes"))
-enem_part$nr_inscritos <- enem_part$nr_participantes+enem_part$nr_nao_participantes
+enem2017$fl_inscrito <- 1
+enem2017$fl_ausente_dia1 <- ifelse(enem2017$TP_PRESENCA_CH==0,1,0)
+enem2017$fl_ausente_dia2 <- ifelse(enem2017$TP_PRESENCA_CN==0,1,0)
+enem2017$fl_participante <- ifelse(enem2017$TP_PRESENCA_CN==1 & enem2017$TP_PRESENCA_CH==1,1,0)
+enem2017$fl_anulada <- ifelse(enem2017$fl_participante==0 & (enem2017$TP_PRESENCA_CH==2 | enem2017$TP_PRESENCA_CN==2),1,0)
 
-# Alinha base de municípios
-regioes <- data.frame(id_regiao=c(1,2,3,4,5),nm_regiao=c("Norte","Nordeste","Sudeste","Sul","Centro-Oeste"))
-municipios2017 <- setNames(municipios2017,c("id_uf","nm_uf","sg_uf","id_municipio","nm_municipio"))
-municipios2017$id_regiao <- substring(municipios2017$id_uf,1,1)
-municipios2017 <- merge(municipios2017,regioes,by="id_regiao")
+# Produz os dados de participação usados na qualificação das escolas
+enem_part <- enem2017
+enem_part <- setNames(
+  data.frame(dcast(data.table(enem_part),NU_ANO+CO_ESCOLA~"",value.var=c("fl_inscrito","fl_participante","fl_ausente_dia1","fl_ausente_dia2","fl_anulada"),sum)),
+  c("nr_ano_enem","id_escola","nr_inscritos","nr_participantes","nr_ausencias_dia1","nr_ausencias_dia2","nr_anuladas")
+)
+censo_matriculas <- setNames(dcast(subset(turmas2017,!TP_ETAPA_ENSINO %in% c(29,34)),CO_ENTIDADE~"NU_MATRICULAS",value.var="NU_MATRICULAS",sum),c("id_escola","nr_matriculas"))
+enem_part <- merge(x=enem_part,censo_matriculas,by="id_escola",all.x=T)
+enem_part$pc_participacao_i <- enem_part$nr_participantes/enem_part$nr_inscritos*100
+enem_part$pc_participacao_m <- enem_part$nr_participantes/enem_part$nr_matriculas*100
 
-# Unifica as bases ####
-enem_part <- merge(enem_part,escolas2017,by="id_escola")
-enem_part <- merge(enem_part,municipios2017,by="id_municipio")
+# Sinaliza as escolas válidas
+enem_part$fl_valida <- ifelse(
+  enem_part$nr_participantes >= 10
+  & (
+    (enem_part$pc_participacao_i >= 50 & is.na(enem_part$nr_matriculas)) 
+    | (enem_part$pc_participacao_m >= 50 & !is.na(enem_part$nr_matriculas) & enem_part$nr_matriculas>=20)
+  )
+  ,1,0
+)
 
-# Calcula os percentuais de indscritos e participantes, limitando ambos a 100%
-enem_part$pc_inscricao <- enem_part$nr_inscritos/enem_part$nr_matriculas*100
-enem_part$pc_participacao <- enem_part$nr_participantes/enem_part$nr_matriculas*100
-enem_part$pc_inscricao <- ifelse(enem_part$pc_inscricao>=100,100,enem_part$pc_inscricao)
-enem_part$pc_participacao <- ifelse(enem_part$pc_participacao>=100,100,enem_part$pc_participacao)
-
-# Trata e alinha a base do ENEM calculando as médias para as áreas e para redação das escolas que atingiram o número mínimo de participantes (10) e o percentual mínimo de participação (50)
-enem_result <- subset(enem2017,enem2017$ds_participante=="participante" & CO_ESCOLA %in% subset(enem_part,nr_participantes>=10)$id_escola & CO_ESCOLA %in% subset(enem_part,pc_participacao>=50)$id_escola)
+# Calcula as proficiências para as escolas válidas
+enem_result <- subset(enem2017,CO_ESCOLA %in% subset(enem_part,fl_valida==1)$id_escola)
 enem_result[] <- lapply(lapply(enem_result, as.character),as.numeric)
 enem_result <- setNames(
-	data.frame(dcast(data.table(enem_result),NU_ANO+CO_ESCOLA~"",value.var=c("NU_NOTA_CN","NU_NOTA_CH","NU_NOTA_LC","NU_NOTA_MT","NU_NOTA_COMP1","NU_NOTA_COMP2","NU_NOTA_COMP3","NU_NOTA_COMP4","NU_NOTA_COMP5","NU_NOTA_REDACAO"),mean)),
-	c("nr_ano_enem","id_escola","nr_media_cn","nr_media_ch","nr_media_lc","nr_media_mt","nr_media_comp_1","nr_media_comp_2","nr_media_comp_3","nr_media_comp_4","nr_media_comp_5","nr_media_redacao")
+  data.frame(dcast(data.table(enem_result),NU_ANO+CO_ESCOLA~"",value.var=c("NU_NOTA_CN","NU_NOTA_CH","NU_NOTA_LC","NU_NOTA_MT","NU_NOTA_COMP1","NU_NOTA_COMP2","NU_NOTA_COMP3","NU_NOTA_COMP4","NU_NOTA_COMP5","NU_NOTA_REDACAO"),mean,na.rm=T)),
+  c("nr_ano_enem","id_escola","nr_media_cn","nr_media_ch","nr_media_lc","nr_media_mt","nr_media_comp_1","nr_media_comp_2","nr_media_comp_3","nr_media_comp_4","nr_media_comp_5","nr_media_redacao")
 )	
 enem_result$nr_media_areas <- (enem_result$nr_media_cn+enem_result$nr_media_ch+enem_result$nr_media_lc+enem_result$nr_media_mt)/4
 enem_result$nr_media_geral <- (enem_result$nr_media_cn+enem_result$nr_media_ch+enem_result$nr_media_lc+enem_result$nr_media_mt+enem_result$nr_media_redacao)/5
 
-#
+# Alnha e organiza os dados das escolas
+redes <- data.frame(TP_DEPENDENCIA=c(1,2,3,4),DS_DEPENDENCIA=c("Federal","Estadual","Municipal","Privada"))
+escolas2017 <- merge(escolas2017,redes,by="TP_DEPENDENCIA")
+escolas2017 <- setNames(escolas2017,c("cd_dependencia","id_escola","nm_escola","id_municipio","ds_dependencia"))
 
-# Unifica participação e médias
-enem_escola <- merge(enem_part,enem_result,by="id_escola")
+# Alinha organiza dos dados das Regiões, UF e municípios
+regioes <- data.frame(id_regiao=c(1,2,3,4,5),nm_regiao=c("Norte","Nordeste","Sudeste","Sul","Centro-Oeste"))
+municipios <- setNames(municipios,c("id_uf","nm_uf","sg_uf","id_municipio","nm_municipio"))
+municipios$id_regiao <- substring(municipios$id_uf,1,1)
+municipios <- merge(municipios,regioes,by="id_regiao")
 
-# Ordena variáveis
-enem_escola <- enem_escola[c(
-	"nr_ano_enem","id_regiao","nm_regiao","id_uf","sg_uf","id_municipio","nm_municipio","cd_dependencia","ds_dependencia","id_escola","nm_escola",
-	"nr_matriculas","nr_inscritos","nr_participantes","pc_inscricao","pc_participacao",
-	"nr_media_cn","nr_media_ch","nr_media_lc","nr_media_mt","nr_media_comp_1","nr_media_comp_2","nr_media_comp_3","nr_media_comp_4","nr_media_comp_5","nr_media_redacao","nr_media_areas","nr_media_geral"
-	)]
+# Unifica os dados de participação, médias, escolas e municípios
+enem_escola <- merge(x=enem_part,enem_result,by=c("nr_ano_enem","id_escola"),all.x=T)
+enem_escola <- merge(enem_escola,escolas2017,by="id_escola")
+enem_escola <- merge(enem_escola,municipios,by="id_municipio")
 
-# Exporta resultado para CSV
+# Seleciona apenas os dados das escolas válidas
+enem_escola <- 
+  subset(
+    enem_escola,
+    fl_valida==1,
+    select=c(
+      "nr_ano_enem","id_regiao","nm_regiao","id_uf","sg_uf","id_municipio","nm_municipio","cd_dependencia","ds_dependencia","id_escola","nm_escola","nr_inscritos","nr_participantes","pc_participacao_i",
+      "nr_media_cn","nr_media_ch","nr_media_lc","nr_media_mt","nr_media_areas","nr_media_comp_1","nr_media_comp_2","nr_media_comp_3","nr_media_comp_4","nr_media_comp_5","nr_media_redacao"
+    )
+  )
+
+# Exporta dados para CSV
 write.csv2(enem_escola,"enem_por_escola_2017.csv",row.names=F,na="")
-
